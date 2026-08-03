fd0e97f743c4303530b8530a089a134be6d88185
M Septyo Al B P <septyo@gamgee.io>
2026-08-03 14:22:25 +0800

    Secure Pages publishing and add CAPTCHA flow

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+// Public runtime configuration for the static site. Never put secrets here.
+window.GAMGEE_CONFIG = Object.freeze({
+  formEndpoint: 'https://script.google.com/macros/s/AKfycbzbH_xCsbTZPU1uOAYQxtywn_MbEd1EUDhqlNKRc0gmRiSFa2syYiKZxJv0WzlNZ-cv/exec',
+  recaptchaSiteKey: '6LdY5XItAAAAAGY7IT5d5B0lTY2T1gEBfOZSLzit'
+});
